a4d920196873d2942de53087fcc20ed9d44d6c2f
RJ Cabrera <robertjohncabrera045@gmail.com>
2023-07-23 20:23:54 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# World Explorer
+
+[World Explorer](https://robcabrera14.github.io/world-explorer/) is responsive website using HTML, CSS and JavaScript
---
+++ assets/js/app.js
@@ -0,0 +1,3 @@
+const navbarToggleBtn = document.querySelector('.navbar-toggle-btn').addEventListener('click', function () {
+  const navbarMenu = document.querySelector('.navbar-menu').classList.toggle('hidden');
+});
